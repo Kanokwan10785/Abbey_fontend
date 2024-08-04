6df12c53d446d6542a93f19ac56933bcaf1eccca
Aeknaronk Thanomchon <6410110613@psu.ac.th>
2024-08-05 00:49:03 +0700

    FoodScreen_v.2

--- app/Screens/FoodScreen.js
+++ app/Screens/FoodScreen.js
@@ -9,17 +9,129 @@ import empty from '../../assets/image/Clothing-Icon/empty-icon-01.png';
 import fruit from '../../assets/image/fruit-01.png';
 import cross from '../../assets/image/Clothing-Icon/cross-icon-01.png';
 
-const foodData = [
-  { id: 1, image: require('../../assets/image/Clothing-Item/Food/F01.png'), name: 'F01', quantity: 2 },
-  { id: 2, image: require('../../assets/image/Clothing-Item/Food/F02.png'), name: 'F02', quantity: 2 },
-  { id: 3, image: require('../../assets/image/Clothing-Item/Food/F03.png'), name: 'F03', quantity: 2 },
-  { id: 4, image: require('../../assets/image/Clothing-Item/Food/F04.png'), name: 'F04', quantity: 2 },
-  { id: 5, image: require('../../assets/image/Clothing-Item/Food/F05.png'), name: 'F05', quantity: 2 },
+const initialFoodData = [
+  { id: 1, image: require('../../assets/image/Clothing-Item/Food/F01.png'), name: 'F01', quantity: 12 },
+  { id: 2, image: require('../../assets/image/Clothing-Item/Food/F02.png'), name: 'F02', quantity: 0 },
+  { id: 3, image: require('../../assets/image/Clothing-Item/Food/F03.png'), name: 'F03', quantity: 0 },
+  { id: 4, image: require('../../assets/image/Clothing-Item/Food/F04.png'), name: 'F04', quantity: 8 },
+  { id: 5, image: require('../../assets/image/Clothing-Item/Food/F05.png'), name: 'F05', quantity: 0 },
   { id: 6, image: require('../../assets/image/Clothing-Item/Food/F06.png'), name: 'F06', quantity: 0 },
 ];
 
+const defaultPetImages = {
+  S00P00K00: require('../../assets/image/Food-Pet/S00P00K00.gif'),
+  S00P00K01: require('../../assets/image/Food-Pet/S00P00K01.gif'),
+  S00P01K00: require('../../assets/image/Food-Pet/S00P01K00.gif'),
+  S00P01K01: require('../../assets/image/Food-Pet/S00P01K01.gif'),
+  S00P02K00: require('../../assets/image/Food-Pet/S00P02K00.gif'),
+  S00P02K01: require('../../assets/image/Food-Pet/S00P02K01.gif'),
+  S01P00K00: require('../../assets/image/Food-Pet/S01P00K00.gif'),
+  S01P00K01: require('../../assets/image/Food-Pet/S01P00K01.gif'),
+  S01P01K00: require('../../assets/image/Food-Pet/S01P01K00.gif'),
+  S01P01K01: require('../../assets/image/Food-Pet/S01P01K01.gif'),
+  S01P02K00: require('../../assets/image/Food-Pet/S01P02K00.gif'),
+  S01P02K01: require('../../assets/image/Food-Pet/S01P02K01.gif'),
+  S02P00K00: require('../../assets/image/Food-Pet/S02P00K00.gif'),
+  S02P00K01: require('../../assets/image/Food-Pet/S02P00K01.gif'),
+  S02P01K00: require('../../assets/image/Food-Pet/S02P01K00.gif'),
+  S02P01K01: require('../../assets/image/Food-Pet/S02P01K01.gif'),
+  S02P02K00: require('../../assets/image/Food-Pet/S02P02K00.gif'),
+  S02P02K01: require('../../assets/image/Food-Pet/S02P02K01.gif'),
+};
+
+const eatingPetImages = {
+  S00P00K00F01: require('../../assets/image/Food-Pet/S00P00K00F01.gif'),
+  S00P00K01F01: require('../../assets/image/Food-Pet/S00P00K01F01.gif'),
+  S00P01K00F01: require('../../assets/image/Food-Pet/S00P01K00F01.gif'),
+  S00P01K01F01: require('../../assets/image/Food-Pet/S00P01K01F01.gif'),
+  S00P02K00F01: require('../../assets/image/Food-Pet/S00P02K00F01.gif'),
+  S00P02K01F01: require('../../assets/image/Food-Pet/S00P02K01F01.gif'),
+  S01P00K00F01: require('../../assets/image/Food-Pet/S01P00K00F01.gif'),
+  S01P00K01F01: require('../../assets/image/Food-Pet/S01P00K01F01.gif'),
+  S01P01K00F01: require('../../assets/image/Food-Pet/S01P01K00F01.gif'),
+  S01P01K01F01: require('../../assets/image/Food-Pet/S01P01K01F01.gif'),
+  S01P02K00F01: require('../../assets/image/Food-Pet/S01P02K00F01.gif'),
+  S01P02K01F01: require('../../assets/image/Food-Pet/S01P02K01F01.gif'),
+  S02P00K00F01: require('../../assets/image/Food-Pet/S02P00K00F01.gif'),
+  S02P00K01F01: require('../../assets/image/Food-Pet/S02P00K01F01.gif'),
+  S02P01K00F01: require('../../assets/image/Food-Pet/S02P01K00F01.gif'),
+  S02P01K01F01: require('../../assets/image/Food-Pet/S02P01K01F01.gif'),
+  S02P02K00F01: require('../../assets/image/Food-Pet/S02P02K00F01.gif'),
+  
+  S00P00K00F04: require('../../assets/image/Food-Pet/S00P00K00F04.gif'),
+  S00P00K01F04: require('../../assets/image/Food-Pet/S00P00K01F04.gif'),
+  S00P01K00F04: require('../../assets/image/Food-Pet/S00P01K00F04.gif'),
+  S00P01K01F04: require('../../assets/image/Food-Pet/S00P01K01F04.gif'),
+  S00P02K00F04: require('../../assets/image/Food-Pet/S00P02K00F04.gif'),
+  S00P02K01F04: require('../../assets/image/Food-Pet/S00P02K01F04.gif'),
+  S01P00K00F04: require('../../assets/image/Food-Pet/S01P00K00F04.gif'),
+  S01P00K01F04: require('../../assets/image/Food-Pet/S01P00K01F04.gif'),
+  S01P01K00F04: require('../../assets/image/Food-Pet/S01P01K00F04.gif'),
+  S01P01K01F04: require('../../assets/image/Food-Pet/S01P01K01F04.gif'),
+  S01P02K00F04: require('../../assets/image/Food-Pet/S01P02K00F04.gif'),
+  S01P02K01F04: require('../../assets/image/Food-Pet/S01P02K01F04.gif'),
+  S02P00K00F04: require('../../assets/image/Food-Pet/S02P00K00F04.gif'),
+  S02P00K01F04: require('../../assets/image/Food-Pet/S02P00K01F04.gif'),
+  S02P01K00F04: require('../../assets/image/Food-Pet/S02P01K00F04.gif'),
+  S02P01K01F04: require('../../assets/image/Food-Pet/S02P01K01F04.gif'),
+  S02P02K00F04: require('../../assets/image/Food-Pet/S02P02K00F04.gif'),
+};
+
 export default function FoodScreen({ navigation }) {
-  const { selectedItems, setSelectedItems } = useContext(ClothingContext);
+  const { selectedItems } = useContext(ClothingContext);
+  const [foodData, setFoodData] = useState(initialFoodData);
+  const [currentPetImage, setCurrentPetImage] = useState(null);
+  const [isEating, setIsEating] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    updatePetImage();
+  }, [selectedItems]);
+
+  useEffect(() => {
+    if (isEating) {
+      const { shirt, pant, skin } = selectedItems;
+      const shirtName = shirt ? shirt.name : 'S00';
+      const pantName = pant ? pant.name : 'P00';
+      const skinName = skin ? skin.name : 'K00';
+      setTimeout(() => {
+        setCurrentPetImage(`${shirtName}${pantName}${skinName}`);
+        setIsEating(false);
+        setIsButtonDisabled(false); // Enable button after 2.8 seconds
+      }, 2800);
+    }
+  }, [isEating]);
+
+  const handleEat = (item) => {
+    if (item.quantity <= 0 || isButtonDisabled) {
+      return;
+    }
+    setIsButtonDisabled(true); // Disable button after clicking "กิน"
+    const { shirt, pant, skin } = selectedItems;
+    const shirtName = shirt ? shirt.name : 'S00';
+    const pantName = pant ? pant.name : 'P00';
+    const skinName = skin ? skin.name : 'K00';
+    const petKey = `${shirtName}${pantName}${skinName}${item.name}`;
+
+    setCurrentPetImage(petKey);
+    setIsEating(true);
+
+    // Update foodData to reduce quantity
+    setFoodData(prevFoodData =>
+      prevFoodData.map(foodItem =>
+        foodItem.id === item.id ? { ...foodItem, quantity: foodItem.quantity - 1 } : foodItem
+      )
+    );
+  };
+
+  const updatePetImage = () => {
+    const { shirt, pant, skin } = selectedItems;
+    const shirtName = shirt ? shirt.name : 'S00';
+    const pantName = pant ? pant.name : 'P00';
+    const skinName = skin ? skin.name : 'K00';
+
+    setCurrentPetImage(`${shirtName}${pantName}${skinName}`);
+  };
 
   const renderItems = () => {
     return (
@@ -28,7 +140,7 @@ export default function FoodScreen({ navigation }) {
           <View key={item.id} style={styles.item}>
             <Image source={item.image ? item.image : empty} style={styles.itemImage} />
             <Text style={styles.itemQuantity}>{item.quantity}</Text>
-            <TouchableOpacity style={styles.itemButton}>
+            <TouchableOpacity style={styles.itemButton} onPress={() => handleEat(item)} disabled={isButtonDisabled}>
               <Text style={styles.itemButtonText}>กิน</Text>
             </TouchableOpacity>
           </View>
@@ -46,14 +158,14 @@ export default function FoodScreen({ navigation }) {
             <DollarIcon />
           </View>
           <View style={styles.petDisplay}>
-            <Image source={selectedItems.mypet ? selectedItems.mypet : empty} style={styles.petImage} />
+            <Image source={isEating ? eatingPetImages[currentPetImage] : defaultPetImages[currentPetImage]} style={styles.petImage} />
           </View>
         </ImageBackground>
       </View>
       <View style={styles.orangeBackground}>
-          <TouchableOpacity style={styles.crossButton} onPress={() => navigation.navigate('HomeScreen')}>
-            <Image source={cross} style={styles.crossIcon} />
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.crossButton} onPress={() => navigation.navigate('HomeScreen')}>
+          <Image source={cross} style={styles.crossIcon} />
+        </TouchableOpacity>
         <View style={styles.wardrobeMenu}>
           <Image source={fruit} style={styles.wardrobeIcon} />
           <Text style={styles.wardrobeText}>อาหาร</Text>
@@ -74,14 +186,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   gymBackgroundContainer: {
-    flex: 1, 
+    flex: 1,
   },
   gymBackground: {
     flex: 1,
     resizeMode: "cover",
   },
   orangeBackground: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: "#FFAF32",
   },
   header: {
@@ -91,12 +203,11 @@ const styles = StyleSheet.create({
   },
   petDisplay: {
     flex: 2,
-    // justifyContent: "center",
     alignItems: "center",
   },
   petImage: {
-    width: 450,
-    height: 450,
+    width: "100%",
+    height: "100%",
     resizeMode: "contain",
   },
   wardrobeIcon: {
@@ -124,7 +235,6 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: -4,
     top: -4,
-    // backgroundColor: "#000",
   },
   crossIcon: {
     width: 30,
@@ -132,7 +242,7 @@ const styles = StyleSheet.create({
   },
   itemsBackground: {
     backgroundColor: "#9E640A",
-    zIndex: 1, // เพิ่ม zIndex ให้ต่ำกว่าของ collectionMenu
+    zIndex: 1,
     flex: 1,
     marginBottom: -20,
   },
@@ -159,12 +269,11 @@ const styles = StyleSheet.create({
   itemImage: {
     width: 60,
     height: 60,
-    // resizeMode: "contain",
   },
   itemQuantity: {
     fontSize: 16,
     bottom: 12,
-    color: "#FFF",
+    color: "#000",
     position: "absolute",
     fontFamily: "appfont_02",
   },
@@ -177,7 +286,6 @@ const styles = StyleSheet.create({
     bottom: -20,
     width: '80%',
     alignItems: "center",
-    // position: "absolute",
   },
   itemButtonText: {
     fontSize: 14,
