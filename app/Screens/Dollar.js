b3300ed919d7ebbdc287fea83440e9458d919f27
6410110004_kanokwan <6410110004.psu.ac.th>
2024-08-23 22:40:18 +0700

    DollarIcon

--- app/Screens/Dollar.js
+++ app/Screens/Dollar.js
@@ -1,15 +1,42 @@
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Image } from 'react-native';
-import React from 'react';
 import dollar from '../../assets/image/dollar-01.png';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { fetchUserProfile } from './api'; // Import ฟังก์ชันที่ใช้ดึงข้อมูลจาก API
 
-const DollarIcon = () => (
+const DollarIcon = () => {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const loadUserProfile = async () => {
+      const token = await AsyncStorage.getItem('jwt');
+      const userId = await AsyncStorage.getItem('userId');
+      if (!token || !userId) return;
+
+      try {
+        const userData = await fetchUserProfile(userId, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setBalance(userData.balance); // อัปเดตค่า balance จากข้อมูลที่ได้รับ
+      } catch (error) {
+        console.error("Error fetching user profile", error);
+      }
+    };
+
+    loadUserProfile();
+  }, []); // useEffect จะทำงานเพียงครั้งเดียวเมื่อคอมโพเนนต์ถูกโหลด
+
+  return (
     <View style={styles.currencyContainer}>
       <View style={styles.currencyBackground}>
-        <Text style={styles.currencyText}>1,250</Text>
+      <Text style={styles.currencyText}> {balance}</Text>
         <Image source={dollar} style={styles.currencyIcon} />
       </View>
     </View>
-);
+  );
+};
 
   
 const styles = StyleSheet.create({
@@ -21,13 +48,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 8,
     paddingVertical: 5,
-    paddingHorizontal: 25,
+    paddingHorizontal: 24,
     justifyContent: 'center',
-    alignItems: 'center',
   },
-    currencyText: {
+  currencyText: {
     fontSize: 18,
-    marginRight: 14,
+    marginRight: 16,
     fontFamily: 'appfont_02',
   },
   currencyIcon: {
